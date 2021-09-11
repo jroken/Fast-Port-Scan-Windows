1ea2fdcfdbc6005e6152d201da7f785780a2bc3e
jroken <83027391+jroken@users.noreply.github.com>
2021-09-11 11:47:01 +0300

    Update fastscan.py

--- fastscan.py
+++ fastscan.py
@@ -3,5 +3,7 @@ while True:
     os.system("cls")
     ip = input("Enter Website Or İp :  ")
     os.system(f"nmap {ip}")
+    print("")
+    print("Press enter to continue")
     input("")
     os.system("cls")
